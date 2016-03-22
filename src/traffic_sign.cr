35a1edcdf30002098b9de8eb7148180ce6869892
Joshua Lockhart <jlockhart@ncsasports.org>
2016-03-22 18:40:34 -0500

    from ecr to slang templates

--- src/traffic_sign.cr
+++ src/traffic_sign.cr
@@ -1,24 +1,25 @@
 require "kemal"
+require "kilt/slang"
 require "./database/*"
 
 module TrafficSign
-  get "/" do |env|
-    render "src/views/index.ecr", "src/views/layouts/application.ecr"
+  get "/" do
+    render "src/views/index.slang", "src/views/layouts/application.ecr"
   end
 
   get "/project/show" do
-    render "src/views/index.ecr", "src/views/layouts/application.ecr"
+    render "src/views/index.slang", "src/views/layouts/application.ecr"
   end
 
   get "/project/new" do
-    render "src/views/new.ecr", "src/views/layouts/application.ecr"
+    render "src/views/new.slang", "src/views/layouts/application.ecr"
   end
 
   post "/project/create" do
   end
 
   get "/project/edit" do
-    render "src/views/edit.ecr", "src/views/layouts/application.ecr"
+    render "src/views/edit.slang", "src/views/layouts/application.ecr"
   end
 
   patch "/project/update" do
