ae8e2f9f1c3b9a41604d59be25c6ff5b8dc7e084
Joshua Lockhart <jlockhart@ncsasports.org>
2016-03-22 14:50:20 -0500

    adding sqlite removing pg and setting CRUD actions

---
+++ src/database/connection.cr
@@ -0,0 +1,104 @@
+require "../../libs/sqlite3"
+
+module Database::Connection
+  INSERT = "INSERT INTO "
+  FIND = "SELECT * FROM "
+  DESTROY = "DELETE FROM "
+  DUPLICATE = "A #{MODEL} with that attribute exists already"
+
+  def self.connect(command)
+    db = SQLite3::Database.new("traffic_sign.db")
+    results = Array(Int32 | String | Nil).new
+    stmt = db.prepare(command)
+    result_set = stmt.execute
+    while result_set.next
+      results = result_set.to_a
+    end
+    stmt.close
+    db.close
+
+    return results
+  end
+
+  class Project
+    # name, team_id, due_date, adjusted_due_date, status_id, completed
+    MODEL = "projects"
+
+    def self.table
+      Database::Connection.connect("CREATE TABLE projects (id INTEGER PRIMARY KEY ASC, name, due_date, adjusted_due_date, status_id, completed);")
+    end
+
+    def self.create(name = "")
+      check = find(name: name)
+
+      unless check.size > 0
+        statement = INSERT + MODEL + " (name) VALUES('#{name}');"
+        Database::Connection.connect(statement)
+      else
+        p DUPLICATE
+      end
+    end
+
+    def self.find(name = "", id = 0)
+      statement = FIND + MODEL +  " WHERE ;"
+      Database::Connection.connect(statement)
+    end
+
+    def self.destroy
+      statement = DESTROY + MODEL +  " WHERE name = '#{name}';"
+      Database::Connection.connect(statement)
+    end
+  end
+
+  class Team
+    # name
+
+    MODEL = "teams"
+
+    def self.table
+      Database::Connection.connect("CREATE TABLE teams (id INTEGER PRIMARY KEY ASC, name);")
+    end
+
+    def self.create(user_id = 0, card_number = 0)
+      check = find(user_id: user_id, card_number: card_number)
+
+      unless check.size > 0
+      else
+        p DUPLICATE
+      end
+    end
+
+    def self.find
+    end
+
+    def self.destroy
+    end
+  end
+
+
+  class Status
+    #name
+
+    MODEL = "statuses"
+
+    def self.table
+      Database::Connection.connect("CREATE TABLE statuses (id INTEGER PRIMARY KEY ASC, name);")
+    end
+
+    def self.create
+      check = find(name)
+
+      unless check.size > 0
+      else
+        p DUPLICATE
+      end
+    end
+
+    def self.find
+    end
+
+    def self.destroy
+    end
+  end
+
+end
--- src/traffic_sign.cr
+++ src/traffic_sign.cr
@@ -1,9 +1,31 @@
 require "kemal"
-# require "kemal-pg"
+require "./database/*"
 
 get "/" do
   render "src/views/index.ecr"
 end
 
+get "/project/show" do
+  render "src/views/index.ecr"
+end
+
+get "/project/new" do
+  render "src/views/index.ecr"
+end
+
+get "/project/create" do
+end
+
+get "/project/edit" do
+  render "src/views/index.ecr"
+end
+
+get "/project/update" do
+
+end
+
+get "/project/destroy" do
+
+end
 
 Kemal.run
