4e4dc2d0994d4150b80a5a4d4ffa8c4e4e6221ac
Joshua Lockhart <jlockhart@ncsasports.org>
2016-04-11 08:10:13 -0500

    adding scaffolding for routes

--- src/traffic_sign.cr
+++ src/traffic_sign.cr
@@ -1,5 +1,4 @@
 require "kemal"
-require "kilt/slang"
 require "./database/*"
 
 module TrafficSign
@@ -7,13 +6,40 @@ module TrafficSign
     render "src/views/index.ecr", "src/views/layouts/application.ecr"
   end
 
-  post "/project/create" do |params|
+  post "/project/create" do |env|
+    params = env.params
+    # perform some action with the params and redirect back to root
+    env.redirect "/"
   end
 
-  patch "/project/update" do |params|
+  patch "/project/update" do |env|
+    params = env.params
+    # perform some action with the params and redirect back to root
+    env.redirect "/"
   end
 
-  delete "/project/destroy" do |params|
+  delete "/project/destroy" do |env|
+    params = env.params
+    # perform some action with the params and redirect back to root
+    env.redirect "/"
+  end
+
+  post "/team/create" do |env|
+    params = env.params
+    # perform some action with the params and redirect back to root
+    env.redirect "/"
+  end
+
+  patch "/team/update" do |env|
+    params = env.params
+    # perform some action with the params and redirect back to root
+    env.redirect "/"
+  end
+
+  delete "/team/destroy" do |env|
+    params = env.params
+    # perform some action with the params and redirect back to root
+    env.redirect "/"
   end
 end
 
